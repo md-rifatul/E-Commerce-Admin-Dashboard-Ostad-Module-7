0021a740adbae23cb55cc89f0f66d77c5f25e453
Md. Rifatul Islam <rifatuul@gmail.com>
2025-12-24 21:35:33 +0600

    updated

---
+++ E-Commerce Admin Dashboard/Models/ProductPageViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace E_Commerce_Admin_Dashboard.Models
+{
+    public class ProductPageViewModel
+    {
+        public List<Product> Products { get; set; }
+        public Product NewProduct { get; set; }
+    }
+}
