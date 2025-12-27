af7fd828585b803bf21b9d43386dc2b08e31fe0c
Md. Rifatul Islam <rifatuul@gmail.com>
2025-12-27 20:34:47 +0600

    delete unnecessary class

--- E-Commerce Admin Dashboard/Models/ProductPageViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace E_Commerce_Admin_Dashboard.Models
-{
-    public class ProductPageViewModel
-    {
-        public List<Product> Products { get; set; }
-        public Product NewProduct { get; set; }
-    }
-}
